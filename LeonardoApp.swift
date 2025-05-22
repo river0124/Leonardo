c1b57c14b7f2e4be2a8d09187406be094fb292e9
river0124 <river94@me.com>
2025-05-22 19:06:22 +0900

    Initial commit

---
+++ LeonardoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeonardoApp.swift
+//  Leonardo
+//
+//  Created by Hyung Seok Lee on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct LeonardoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
